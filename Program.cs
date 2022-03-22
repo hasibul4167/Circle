53caabc8fd0d8f1dc1a3b2020166057d173b6ac6
Md Hasibul Islam <hasibulislam3409@gmail.com>
2022-03-22 11:23:36 +0600

    Circle

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Cricle
+{
+    class Program
+    {
+        static void Main(string[] arrays)
+        {
+            double area, redious;
+            System.Console.WriteLine("Enter Redious Number - ");
+            redious = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * redious * redious;
+            System.Console.WriteLine("Area is Cricle - " + area);
+            Console.ReadKey();
+        }
+    }
+}
